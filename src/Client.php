4849cff9e3c1bedf645211d036c2995715fce04b
Raffael Tunder <raffael.tunder@iamstudent.at>
2019-09-04 15:42:49 +0200

    remove createBearerToken

--- src/Client.php
+++ src/Client.php
@@ -68,26 +68,4 @@ class Client
 
         return $response;
     }
-
-    public static function getVoucherById(): ResponseInterface
-    {
-        $client = HttpClient::create();
-
-        $response = $client->request('GET', 'https://api2.dev.iamstudent.at/api/v1/voucher/15');
-
-        return $response;
-    }
-
-    public static function login(): ResponseInterface
-    {
-        $client = HttpClient::create();
-
-        $body = ['username' => 'raffael.tunder@iamstudent.at', 'password' => 'mypassword'];
-
-        $response = $client->request('POST', 'https://api2.dev.iamstudent.at/api/login_check', [
-            'json' => $body
-        ]);
-
-        return $response;
-    }
 }
